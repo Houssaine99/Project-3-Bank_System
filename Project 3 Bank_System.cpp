9dad3087e74c7ea1622a495bfc59025edee1cdaa
Houssaine99 <elhoussaineambarki@gmail.com>
2026-03-22 00:39:33 +0000

    commit Project 3 Bank_System.cpp file because it have been commited with last commit

--- Project 3 Bank_System.cpp
+++ Project 3 Bank_System.cpp
@@ -9,44 +9,7 @@
 #include "ClsUtil.h"
 #include "clsMainScreen.h"
 
-
-void PrintClientRecordBalanceLine(clsBankClient Client)
-{
-	std::cout << "| " << std::left << std::setw(25) << Client.AccountNumber();
-	std::cout << "| " << std::left << std::setw(58) << Client.FullName();
-	std::cout << "| " << std::left << std::setw(29) << Client.AccountBalance << std::left << std::setw(1) << "|\n";
-}
-
-void ShowTotalBalances()
-{
-	std::vector <clsBankClient> vClients = clsBankClient::GetClientsList();
-	int NumberOfClients = vClients.size();
-	double TotalBalances = 0;
-
-	std::cout << "\n" << std::string(5, '\t') << "Balances List(" << NumberOfClients << ") Client(s)\n";
-
-	std::cout << std::string(119, '-') << "\n";
-	std::cout << "| " << std::left << std::setw(25) << "Account Number";
-	std::cout << "| " << std::left << std::setw(58) << "Client Name";
-	std::cout << "| " << std::left << std::setw(29) << "Account Balance" << std::left << std::setw(1) << "|\n";
-	std::cout << std::string(119, '-') << "\n";
-
-	TotalBalances = clsBankClient::GetTotalBalances();
-
-	if(NumberOfClients == 0)
-		std::cout << "| " << std::string(4, '\t') << "No Clients Available In the System!" << std::right << std::setw(52) << "|" << std::endl;
-	else
-	{
-		for (clsBankClient& Client : vClients)
-			PrintClientRecordBalanceLine(Client);
-	}
-
-	std::cout << std::string(119, '-') << std::endl;
-
-	std::cout << std::string(5, '\t') << "  Total Balances = " << TotalBalances << std::endl;
-	std::cout << std::string(5, '\t') << "  ( " << clsUtil::NumberToText(TotalBalances) << ")\n";
-}
-
+	
 int main()
 {
 	clsMainScreen Main;
